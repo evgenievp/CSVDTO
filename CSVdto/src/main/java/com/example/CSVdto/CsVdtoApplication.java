a74fa4639b4884782ff442848f43cc79b3f017a4
evgenievp <evgenievp@yahoo.com>
2025-04-12 20:47:48 +0300

    Project initialization

---
+++ CSVdto/src/main/java/com/example/CSVdto/CsVdtoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CSVdto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsVdtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsVdtoApplication.class, args);
+	}
+
+}
---
+++ CSVdto/src/test/java/com/example/CSVdto/CsVdtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CSVdto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsVdtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
